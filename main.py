bd7ca982a2b3a96a32611c2a8864a889e35b5542
Rostislav Ivankiv <bbwgd77@gmail.com>
2019-09-14 22:28:26 +0300

    Rename model

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from misc import multiply_vectors, get_truth_table
 
 
 @dataclass
-class BinaryPerceptronDataFrame:
+class BinaryPerceptron:
     weights: list
     inputs: tuple
 
@@ -32,11 +32,11 @@ class BinaryPerceptronDataFrame:
     def correct_weights(self):
         def d_i(x_i): return self.learning_rate * self.delta * x_i
 
-        self.weights = [round(w_i + d_i(x_i), 4)
+        self.weights = [w_i + d_i(x_i)
                         for w_i, x_i in zip(self.weights, self.inputs)]
 
     def __str__(self) -> str:
-        return f'BPDf(ws={self.weights}, ' \
+        return f'BP(ws={self.weights}, ' \
                f'xs={self.inputs}, ' \
                f'a={self.a}, ' \
                f'Y={self.Y},' \
@@ -49,29 +49,29 @@ class BinaryPerceptronModel:
 
     def __init__(self, input_layer: List[tuple]) -> None:
         self.input_layer = input_layer
-        self.dfs = None
+        self.bps = None
 
-    def train(self, learning_rate=0.7) -> List[BinaryPerceptronDataFrame]:
-        self.dfs = self._initial_train(learning_rate)
+    def train(self, learning_rate=0.7) -> List[BinaryPerceptron]:
+        self.bps = self._initial_train(learning_rate)
         while self._bad_trained():
             self._train()
-        return self.dfs
+        return self.bps
 
-    def _initial_train(self, learning_rate) -> List[BinaryPerceptronDataFrame]:
-        return [BinaryPerceptronDataFrame(self.default_weights,
-                                          (x1, x2, x3),
-                                          self.threshold,
-                                          learning_rate,
-                                          expected)
+    def _initial_train(self, learning_rate) -> List[BinaryPerceptron]:
+        return [BinaryPerceptron(self.default_weights,
+                                 (x1, x2, x3),
+                                 self.threshold,
+                                 learning_rate,
+                                 expected)
                 for x1, x2, x3, expected in self.input_layer]
 
     def _bad_trained(self):
-        return any(df_i.delta != 0 for df_i in self.dfs)
+        return any(df_i.delta != 0 for df_i in self.bps)
 
     def _train(self):
-        for df in self.dfs:
-            if df.T != df.Y:
-                df.correct_weights()
+        for p in self.bps:
+            if p.T != p.Y:
+                p.correct_weights()
 
 
 if __name__ == '__main__':
@@ -80,12 +80,12 @@ if __name__ == '__main__':
 
     table = [(m.weights,
               m.inputs,
-              m.threshold,
               m.a,
+              m.threshold,
               m.Y,
               m.T)
              for m in train]
     print(tabulate(table, headers=['weights',
                                    'inputs',
-                                   'threshold',
-                                   'a', 'Y', 'T']))
+                                   'a',
+                                   'threshold', 'Y', 'T']))
